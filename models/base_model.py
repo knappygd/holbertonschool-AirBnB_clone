b2b257827d60d12cdb385f2f61f43188dd08346c
knapp <emiliodamasco04@gmail.com>
2023-07-04 14:05:56 -0700

    added args, kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,27 @@ class BaseModel:
     BaseModel defines all common attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
+        Args:
+            args: Unused
+            kwargs: Arguments for the BaseModel constructor.
+
         Attributes:
             id: unique id generated
             created_at: creation date
             updated_at: updated date
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """ Updates `updated_at` with the current datetime. """
