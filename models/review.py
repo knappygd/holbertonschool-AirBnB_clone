6dd9805edc880f7d112d9ec70845aba65cdc8d44
knapp <emiliodamasco04@gmail.com>
2023-07-06 14:23:57 -0700

    added docs to new classes

--- models/review.py
+++ models/review.py
@@ -4,7 +4,13 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Defines the class Review."""
+    """Defines the class Review.
+
+    Attributes:
+        place_id: The id of the place.
+        user_id: The id of the user.
+        text: The content of the review.
+    """
     place_id = ""
     user_id = ""
     text = ""
