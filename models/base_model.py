96b7db2bf05219fbe75a4e1a9c0b89405b786aca
knapp <emiliodamasco04@gmail.com>
2023-07-06 13:28:17 -0700

    added more classes

--- models/base_model.py
+++ models/base_model.py
@@ -43,7 +43,7 @@ class BaseModel:
         Returns a dictionary containing
         all keys/values of __dict__ of the instance.
         """
-        my_dict = self.__dict__.copy()
+        my_dict = dict(self.__dict__.copy())
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
