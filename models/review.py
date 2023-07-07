0c5914fbd98dcd29d57ae315a2c76624983e6dce
knapp <emiliodamasco04@gmail.com>
2023-07-07 05:49:28 -0700

    added new imports

--- models/review.py
+++ models/review.py
@@ -4,13 +4,7 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Defines the class Review.
-
-    Attributes:
-        place_id: The id of the place.
-        user_id: The id of the user.
-        text: The content of the review.
-    """
+    """Defines the class Review."""
     place_id = ""
     user_id = ""
     text = ""
