ac585939e81051b7d00aef4dac9e16ffae7bc7e1
juandi131 <juandigarcia3@gmail.com>
2023-07-08 01:53:00 -0700

    upload unitest

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Unittest for Test File storage. """
+import json
+import os
+import unittest
+from unittest.mock import patch, mock_open
+from models.base_model import BaseModel
+
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        self.storage = FileStorage()
+
+    def test_all_returns_dict(self):
+        result = self.storage.all()
+        self.assertIsInstance(result, dict)
+
+    def test_new_adds_instance_to_objects(self):
+        obj = BaseModel()
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.storage.new(obj)
+        self.assertIn(key, self.storage.all())
+
+    def test_save_writes_to_file(self):
+        obj = BaseModel()
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.storage.new(obj)
+
+        temp_file = "temp_file.json"
+        self.storage._FileStorage__file_pathkkoijk = temp_file
+
+        self.storage.save()
+
+        with open(temp_file, "r") as file:
+            data = json.load(file)
+            self.assertIn(key, data)
+        os.remove(temp_file)
+
+
+if __name__ == "__main__":
+    unittest.main()
